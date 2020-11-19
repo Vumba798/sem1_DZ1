14e7629ca4353c75a1a13410ee2f71e8685e9aec
Vumba798 <alexandrov32649@gmail.com>
2020-11-20 02:05:27 +0300

    fixed trouble with encoding end-symbol

--- sources/demo.cpp
+++ sources/demo.cpp
@@ -13,7 +13,6 @@ using std::endl;
 
 int main()
 {
-
     bool mode;
     std::string plainText, cipherText;
     unsigned long initKey;
@@ -24,11 +23,13 @@ int main()
     cin >> mode;
 
     //последняя цифра - кол-во бит для сдвига
-    //если предпоследняя цифра - чётная, то сдвиг вправо, если нечётная, то влево if (mode == 1)//Режим шифрования
-    if (mode) {
-        cout << "==========================================" << endl; cout << "ENCRYPTION MODE" << endl <<
-        "==========================================" << endl;
-        cout << "Please, input the initKey(cipher): "; cin.ignore();
+    //если предпоследняя цифра - чётная, то сдвиг вправо, если нечётная, то влево
+    if (mode) { // режим шифрования
+        cout << "==========================================" << endl;
+        cout << "ENCRYPTION MODE" << endl <<
+            "==========================================" << endl;
+        cout << "Please, input the initKey(cipher): ";
+        cin.ignore();
         cin >> initKey;
         srand(initKey);//генерация от ключа инициализации
         cout << "Please, input the text you want to encrypt: ";
@@ -39,13 +40,15 @@ int main()
         initKey /= 10;
 
         if ((plainText.length() % 2) == 1) {  //если у нас длина текста не кратна блокам данных, то дозаполняем его символом
-            plainText += '¤';
+            plainText += '~';
         }
         for (unsigned int i = 0; i < plainText.length();i += 2) {
             textInt = static_cast<unsigned char> (plainText[i]);  //преобразуем символ в число
             textInt <<= 8; //сдвигаем на 8 бит
             textInt += static_cast<unsigned char> (plainText[i + 1]);;
-            cipher = std::rand() % 32767; //генерация шифр-ключа cout << "cipher = " << cipher << endl; textInt = cipher ^ textInt; //поразрядный XOR
+            cipher = std::rand() % 32767; //генерация шифр-ключа 
+            cout << "cipher = " << cipher << endl;
+            textInt = cipher ^ textInt; //поразрядный XOR
             if (initKey % 2 == 0) {  //если символ отвечающий за направление сдвига чётный, то сдвиг вправо
                 textInt = (textInt >> shift) | ((textInt) << (16 - shift));
             }else{  //если нечётный, то влево
@@ -62,7 +65,7 @@ int main()
             "==========================================" << endl;
         cout << "Please, input the key-cipher: ";
         cin >> initKey;
-        srand(initKey);//генерация от суммы двух символов ключа cout << "Please, input the text you want to decrypt: "; cin >> cipherText;
+        srand(initKey);//генерация от суммы двух символов ключа
         cout << "Please, input the text you want to decrypt: ";
         cin.ignore();
         getline(cin, cipherText);
@@ -74,7 +77,8 @@ int main()
             textInt <<= 8;
             tmpInt = static_cast<unsigned char>(cipherText[i + 1]);
             textInt += tmpInt;
-            cipher = std::rand() % 32767; //генерация шифр-ключа cout << "cipher = " << cipher << endl;
+            cipher = std::rand() % 32767; //генерация шифр-ключа 
+            cout << "cipher = " << cipher << endl;
             if (initKey % 2 == 0) {  //если цифра, отвечающая за направление сдвига чётная, то сдвиг наоборот, влево
                 textInt = (textInt << shift) | (textInt) >> (16 - shift);
             }else{  //если нечётная, то наоборот, вправо
@@ -85,7 +89,7 @@ int main()
             tmpInt = textInt << 8;
             plainText += (tmpInt >> 8);//добавляем второй символ
         }
-        if (plainText[plainText.length() - 1] == '¤'){  // Удаляем лишний символ
+        if (plainText[plainText.length() - 1] == '~'){  // Удаляем лишний символ
             plainText.erase(plainText.length() - 1, 1);
         }
         cout << "PlainText is: " << plainText << endl;
